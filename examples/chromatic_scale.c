d800afdf8e0bbb21d7fbfb842418c654e46f817f
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-24 19:38:51 -0700

    feat: add chromatic scale example

---
+++ examples/chromatic_scale.c
@@ -0,0 +1,379 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "musical_duration.h"
+#include "musical_player.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+
+/* USER CODE BEGIN PV */
+
+#define SONG_SIZE 88
+
+note_t chromatic_scale[SONG_SIZE] = {
+  {A0, SIXTEENTH_NOTE},
+  {A0_SHARP, SIXTEENTH_NOTE},
+  {B0, SIXTEENTH_NOTE},
+
+  {C1, SIXTEENTH_NOTE},
+  {C1_SHARP, SIXTEENTH_NOTE},
+  {D1, SIXTEENTH_NOTE},
+  {D1_SHARP, SIXTEENTH_NOTE},
+  {E1, SIXTEENTH_NOTE},
+  {F1, SIXTEENTH_NOTE},
+  {F1_SHARP, SIXTEENTH_NOTE},
+  {G1, SIXTEENTH_NOTE},
+  {G1_SHARP, SIXTEENTH_NOTE},
+  {A1, SIXTEENTH_NOTE},
+  {A1_SHARP, SIXTEENTH_NOTE},
+  {B1, SIXTEENTH_NOTE},
+
+  {C2, SIXTEENTH_NOTE},
+  {C2_SHARP, SIXTEENTH_NOTE},
+  {D2, SIXTEENTH_NOTE},
+  {D2_SHARP, SIXTEENTH_NOTE},
+  {E2, SIXTEENTH_NOTE},
+  {F2, SIXTEENTH_NOTE},
+  {F2_SHARP, SIXTEENTH_NOTE},
+  {G2, SIXTEENTH_NOTE},
+  {G2_SHARP, SIXTEENTH_NOTE},
+  {A2, SIXTEENTH_NOTE},
+  {A2_SHARP, SIXTEENTH_NOTE},
+  {B2, SIXTEENTH_NOTE},
+
+  {C3, SIXTEENTH_NOTE},
+  {C3_SHARP, SIXTEENTH_NOTE},
+  {D3, SIXTEENTH_NOTE},
+  {D3_SHARP, SIXTEENTH_NOTE},
+  {E3, SIXTEENTH_NOTE},
+  {F3, SIXTEENTH_NOTE},
+  {F3_SHARP, SIXTEENTH_NOTE},
+  {G3, SIXTEENTH_NOTE},
+  {G3_SHARP, SIXTEENTH_NOTE},
+  {A3, SIXTEENTH_NOTE},
+  {A3_SHARP, SIXTEENTH_NOTE},
+  {B3, SIXTEENTH_NOTE},
+
+  {C4, SIXTEENTH_NOTE},
+  {C4_SHARP, SIXTEENTH_NOTE},
+  {D4, SIXTEENTH_NOTE},
+  {D4_SHARP, SIXTEENTH_NOTE},
+  {E4, SIXTEENTH_NOTE},
+  {F4, SIXTEENTH_NOTE},
+  {F4_SHARP, SIXTEENTH_NOTE},
+  {G4, SIXTEENTH_NOTE},
+  {G4_SHARP, SIXTEENTH_NOTE},
+  {A4, SIXTEENTH_NOTE},
+  {A4_SHARP, SIXTEENTH_NOTE},
+  {B4, SIXTEENTH_NOTE},
+
+  {C5, SIXTEENTH_NOTE},
+  {C5_SHARP, SIXTEENTH_NOTE},
+  {D5, SIXTEENTH_NOTE},
+  {D5_SHARP, SIXTEENTH_NOTE},
+  {E5, SIXTEENTH_NOTE},
+  {F5, SIXTEENTH_NOTE},
+  {F5_SHARP, SIXTEENTH_NOTE},
+  {G5, SIXTEENTH_NOTE},
+  {G5_SHARP, SIXTEENTH_NOTE},
+  {A5, SIXTEENTH_NOTE},
+  {A5_SHARP, SIXTEENTH_NOTE},
+  {B5, SIXTEENTH_NOTE},
+
+  {C6, SIXTEENTH_NOTE},
+  {C6_SHARP, SIXTEENTH_NOTE},
+  {D6, SIXTEENTH_NOTE},
+  {D6_SHARP, SIXTEENTH_NOTE},
+  {E6, SIXTEENTH_NOTE},
+  {F6, SIXTEENTH_NOTE},
+  {F6_SHARP, SIXTEENTH_NOTE},
+  {G6, SIXTEENTH_NOTE},
+  {G6_SHARP, SIXTEENTH_NOTE},
+  {A6, SIXTEENTH_NOTE},
+  {A6_SHARP, SIXTEENTH_NOTE},
+  {B6, SIXTEENTH_NOTE},
+
+  {C7, SIXTEENTH_NOTE},
+  {C7_SHARP, SIXTEENTH_NOTE},
+  {D7, SIXTEENTH_NOTE},
+  {D7_SHARP, SIXTEENTH_NOTE},
+  {E7, SIXTEENTH_NOTE},
+  {F7, SIXTEENTH_NOTE},
+  {F7_SHARP, SIXTEENTH_NOTE},
+  {G7, SIXTEENTH_NOTE},
+  {G7_SHARP, SIXTEENTH_NOTE},
+  {A7, SIXTEENTH_NOTE},
+  {A7_SHARP, SIXTEENTH_NOTE},
+  {B7, SIXTEENTH_NOTE},
+
+  {C8, SIXTEENTH_NOTE}
+};
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM1_Init(void);
+/* USER CODE BEGIN PFP */
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM1_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    play_song(TIM1, SONG_SIZE, chromatic_scale);
+    HAL_Delay(1000);
+    /* TIM1->PSC = C4;
+    HAL_Delay(1000);
+    TIM1->PSC = G4;
+    HAL_Delay(1000); */
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL15;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 4999;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 99;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
